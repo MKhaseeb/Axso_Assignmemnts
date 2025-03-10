950469ae520c92e45c4877bdfdf794d316086ad3
Mohmmad Khaseeb <114239535+Khaseeb01@users.noreply.github.com>
2025-03-10 15:26:12 +0200

    Making Connections

--- web-fun/JavaScript/Loop Challenges/Loop-Challenges.js
+++ web-fun/JavaScript/Loop Challenges/Loop-Challenges.js
@@ -1,20 +1,58 @@
-    //Display odd numbers 
+// function Displayoddnumbers(){
 
-    for ( var i =1; i <= 20 ; i+=2){
-        if (i & 2 !=0)
-            console.log(i)
+//     for ( var i =1; i <= 20 ; i+=2){
+//         if (i & 2 !=0)
+//             console.log(i)
         
-    }
+//     }
+// }
+// function Decreasingmultiplesof3(){
+//     for (var i = 100 ; i >= 0 ; i--){
+//         if (i % 3 === 0) 
+//             console.log(i)
     
-        //Decreasing multiples of 3 
+//     }
+// }
     
-    for (var i = 100 ; i >= 0 ; i--){
-        if (i % 3 === 0) 
-            console.log(i)
-    
-    }
 
-    for (var i = 4 ; i >= 0.5 ; i++ ){
-        console.log(i)
 
-    }
+// function Printthegivensequence(){
+
+//     var x=[4, 2.5, 1, -0.5, -2, -3.5]
+//     for (let index = 0; index < x.length; index++) {
+//         const element = x[index];
+//         console.log(element)
+//     }
+// }
+
+// function Sigma(){
+//     var sum = 0 
+//     for (let index = 0; index <= 100; index++) {
+//         sum=sum+index
+        
+//     }
+//     console.log(sum)
+// }
+
+// function factorial(){
+//     var sum = 1
+//     for (let index = 1; index <= 12; index++) {
+//         sum=sum*index
+        
+//     }
+//     console.log(sum)
+    
+// }
+
+//     Displayoddnumbers()
+//     console.log("_".repeat(30))
+//     Decreasingmultiplesof3()
+//     console.log("_".repeat(30))
+//     Printthegivensequence()
+//     console.log("_".repeat(30)) 
+//     Sigma()
+//     console.log("_".repeat(30)) 
+//     factorial()
+//     console.log("_".repeat(30)) 
+
+var num =
