e3c16446c6f5ba6f145449b48e41dd19cd0b0212
Mohmmad Khaseeb <Mkhaseeb84@gmail.com>
2025-03-11 03:33:03 +0200

    video-preview

---
+++ web-fun/JavaScript/video-preview/script.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const video = document.querySelector("video");
+
+    video.addEventListener("mouseenter", () => {
+        video.play();
+    });
+
+    video.addEventListener("mouseleave", () => {
+        video.pause();
+
+    });
+});
