c6d3080c2b5aac59e1442891736be53c18622c3c
Mohmmad Khaseeb <Mkhaseeb84@gmail.com>
2025-04-03 13:12:59 +0300

    Flask

---
+++ python-stack/_python/python-fun/Functions Intermediate II.py
@@ -0,0 +1,25 @@
+x = [ [5,2,3], [10,8,9] ] 
+students = [
+    {'first_name':  'Michael', 'last_name' : 'Jordan'},
+    {'first_name' : 'John', 'last_name' : 'Rosales'}
+]
+sports_directory = {
+    'basketball' : ['Kobe', 'Jordan', 'James', 'Curry'],
+    'soccer' : ['Messi', 'Ronaldo', 'Rooney']
+}
+z = [ {'x': 10, 'y': 20} ]
+
+
+x[1][0] = 15
+print(x[1][0])
+
+
+
+def fefefe(list):
+    for i in range (0, len(list) , 1):
+        print(list[i])
+
+
+
+
+fefefe([5,5,5,5])
