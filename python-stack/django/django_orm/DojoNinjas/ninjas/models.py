6b5ca658f95d38abf56d1faa210103ad27207e7b
Mohmmad Khaseeb <Mkhaseeb84@gmail.com>
2025-04-17 23:22:59 +0300

    Nin

--- python-stack/django/django_orm/DojoNinjas/ninjas/models.py
+++ python-stack/django/django_orm/DojoNinjas/ninjas/models.py
@@ -3,7 +3,9 @@ from django.db import models
 class Dojo(models.Model):
     name = models.CharField(max_length=255)
     city = models.CharField(max_length=255)
-    state = models.CharField(max_length=2)
+    state = models.CharField(max_length=255)
+    ceated_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
 
 
@@ -11,3 +13,6 @@ class Dojo(models.Model):
 class Ninjas(models.Model):
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
+    ceated_at = models.DateField(auto_now_add=True)
+    updated_at = models.DateField(auto_now=True)
+    dojoid = models.ForeignKey(Dojo, related_name="ninjas", on_delete=models.CASCADE ,default=1)
