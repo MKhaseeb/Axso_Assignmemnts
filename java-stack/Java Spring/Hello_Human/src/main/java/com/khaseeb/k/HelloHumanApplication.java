1bf41fc3be92e95c74bdc31dec6c3d67f6c87685
Mohmmad Khaseeb <Mkhaseeb84@gmail.com>
2025-05-21 13:38:00 +0300

    Hello

--- java-stack/Java Spring/Hello_Human/src/main/java/com/khaseeb/k/HelloHumanApplication.java
+++ java-stack/Java Spring/Hello_Human/src/main/java/com/khaseeb/k/HelloHumanApplication.java
@@ -1,29 +1,29 @@
-package com.Khaseeb.routes;
+package com.khaseeb.k;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-@RestController
 
-public class DaikichiController {
+@RestController
+@SpringBootApplication
+public class HelloHumanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloHumanApplication.class, args);
+	}
 	@RequestMapping("")
 	public String index() {
-		return "hi";
+		return "try /search?q=example";
 	}
 	
-	@RequestMapping("daikichi")
-	public String daikichi() {
-		return "Welcome";
-	}
-	@RequestMapping("daikichi/today")
-	public String today() {
-		return "Today you will find luck in all your endeavors!";
-	}
-	@RequestMapping("daikichi/tomorrow")
-	public String tomorrow() {
-		return "Tomorrow, an opportunity will arise, so be sure to be open to new ideas!";
-		}
     @RequestMapping("/search")
     public String index(@RequestParam(value = "q", required = false, defaultValue = "nothing") String searchQuery) {
         return "You searched for: " + searchQuery;
 }
 }
+
+	
+
+
---
+++ java-stack/Java Spring/Hello_Human/src/test/java/com/khaseeb/k/Human.java
@@ -0,0 +1,19 @@
+//package com.khaseeb.k;
+//
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RequestParam;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//@RestController
+//public class Human {
+//
+//    @RequestMapping("/")
+//    public String home() {
+//        return "Welcome to the home page!";
+//    }
+//
+//    @RequestMapping("/search")
+//    public String index(@RequestParam(value = "q", required = false, defaultValue = "nothing") String searchQuery) {
+//        return "You searched for: " + searchQuery;
+//    }
+//}
