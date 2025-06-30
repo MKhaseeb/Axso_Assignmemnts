d46e9b2c6d39d16b3406a95f34b3e6f38963e708
Mohmmad Khaseeb <Mkhaseeb84@gmail.com>
2025-06-30 09:56:02 +0300

    ks

--- mern-stack/JavaScript Fundamentals/Destructuring.js
+++ mern-stack/JavaScript Fundamentals/Destructuring.js
@@ -1,19 +1,76 @@
+//=====================1=========================
+const cars = ['Tesla', 'Mercedes', 'Honda'];
+
+const [randomCar] = cars;
+const [, otherRandomCar] = cars;
+
+//Tesla
+//Mercedes
+console.log(randomCar);
+console.log(otherRandomCar);
+
+//====================2=========================
+// const employee = {
+//     name: 'Elon',
+//     age: 47,
+//     company: 'Tesla'
+// };
+
+// const { name: otherName } = employee;
+
+// //Error: Elon is not defined
+
+// console.log(name);
+// console.log(otherName);
+
+//====================3=========================
 const person = {
-    firstName: 'Bob',
-    lastName: 'Marley',
-    email: 'bob@marley.com',
-    password: 'sekureP@ssw0rd9',
-    username: 'barley',
-    createdAt: 1543945177623
+    name: 'Phil Smith',
+    age: 47,
+    height: '6 feet'
 };
 
-const animals = [
-    'horse',
-    'dog',
-    'fish',
-    'cat',
-    'bird'
-];
+const password = '12345';
 
-const { email,password }= person;
-console.log(bataat);
+const { password: hashedPassword } = person;
+
+// 12345
+// undefined
+
+console.log(password);
+console.log(hashedPassword);
+
+//====================4=========================
+const numbers = [8, 2, 3, 5, 6, 1, 67, 12, 2];
+
+const [, first] = numbers;
+
+const [,,, second] = numbers;
+
+const [,,,,,,,, third] = numbers;
+
+//false
+//true
+
+
+console.log(first == second);
+console.log(first == third);
+
+//====================5=========================
+const lastTest = {
+    key: 'value',
+    secondKey: [1, 5, 1, 8, 3, 3]
+};
+
+const { key } = lastTest;
+const { secondKey } = lastTest;
+const [, willThisWork] = secondKey;
+
+//value
+//1
+//yes
+
+console.log(key);
+console.log(secondKey);
+console.log(secondKey[0]);
+console.log(willThisWork);
