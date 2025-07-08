d2e5aac77ef11a3af8434f3e1deb14f93655e900
Mohmmad Khaseeb <Mkhaseeb84@gmail.com>
2025-07-08 16:38:42 +0300

    Express

---
+++ mern-stack/Express/FakeAPI/server.js
@@ -0,0 +1,51 @@
+
+const express = require("express");
+const{faker}=require('@faker-js/faker')
+const app = express();
+const port = 8000;
+// req is short for request
+// res is short for response
+
+app.use( express.json() );
+app.use( express.urlencoded({ extended: true }) );
+
+
+const createUser = () => {
+  const newFakeUser = {
+    _id: faker.string.uuid(),
+    fisrtName: faker.person.firstName(),
+    lastName:faker.person.lastName(),
+    phoneNumber:faker.phone.number(),
+    email: faker.internet.email(),
+    password: faker.internet.password()
+  };
+  return newFakeUser;
+};
+
+const newFakeUser = createUser();
+app.get("/api/user", (req, res) => {
+    res.json( newFakeUser );
+});
+
+const createCompany = () => {
+  const newFakeCompany = {
+    _id: faker.string.uuid(),
+    name: faker.company.name(),
+    address:faker.location.streetAddress(),
+    city:faker.location.city(),
+    state:faker.location.state(),
+    zipCode:faker.location.zipCode(),
+    country:faker.location.country(),
+  };
+  return newFakeCompany;
+};
+
+const newFakeCompany = createCompany();
+app.get("/api/company", (req, res) => {
+    res.json( newFakeCompany );
+});
+
+
+const server = app.listen(8000, () =>
+  console.log(`Server is locked and loaded on port ${server.address().port}!`)
+);
