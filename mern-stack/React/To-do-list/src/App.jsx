227f781771047dc1dbe84433c50b2d0f2865d68d
Mohmmad Khaseeb <Mkhaseeb84@gmail.com>
2025-07-11 13:20:18 +0300

    To-do-list

--- mern-stack/React/To-do-list/src/App.jsx
+++ mern-stack/React/To-do-list/src/App.jsx
@@ -1,15 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
 import './App.css'
 import HomeComponent from './components/HomeComponent'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
 
   return (
     <>
-  <HomeComponent/>
+      <HomeComponent/>
     </>
   )
 }
--- mern-stack/React/To-do-list/src/components/HomeComponent.jsx
+++ mern-stack/React/To-do-list/src/components/HomeComponent.jsx
@@ -1,8 +1,65 @@
 import React from 'react'
+import { useState } from 'react'
 
-const HomeComponent = () => {
-    return (
-        <div>HomeComponent</div>
+let nextId = 0;
+
+const HomeComponent = () =>{
+const [todo , setTodo] = useState('')
+const [task, setTask] = useState([])
+
+
+const addTask = () =>{
+    setTask([
+        ...task,
+        {id: nextId++, todo : todo, complete: false}
+    ])
+    setTodo('')
+
+}
+
+const taskComp = (id) => {
+    setTask(task.map(task =>
+        task.id === id ? { ...task, complete: !task.complete } : task
+    ))
+
+
+}
+const deleteTask =(id)=>{
+    setTask(
+        task.filter(e =>
+            e.id !== id
+        ))}
+
+    return(
+        <>
+        <input type="text" 
+            value={todo}
+            onChange={e => setTodo(e.target.value)}
+        />
+        
+        <button
+        onClick={addTask}
+        >
+            Add Task
+        </button>
+        <ul>
+            {task.map(task => (
+                <li key={task.id} >
+                    <label style={{ textDecoration: task.complete ? 'line-through' : 'none' }}>
+                        <input
+                            type="checkbox"
+                            checked={task.complete}
+                            onChange={() => taskComp(task.id)}
+                        />
+                        {task.todo}
+                    </label>
+                <button onClick={() => deleteTask(task.id)}>delete</button>
+                
+                </li>
+            ))}
+                
+        </ul>
+        </>
     )
 }
 
