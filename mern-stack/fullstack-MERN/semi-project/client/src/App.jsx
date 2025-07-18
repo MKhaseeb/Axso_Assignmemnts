497ccc64e52640e9e2312f9e6c7b4fd605f26235
Mohmmad Khaseeb <Mkhaseeb84@gmail.com>
2025-07-18 14:00:52 +0300

    semi-project

--- mern-stack/fullstack-MERN/semi-project/client/src/App.jsx
+++ mern-stack/fullstack-MERN/semi-project/client/src/App.jsx
@@ -3,12 +3,22 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import SidenavComponenet from './components/SidenavComponenet'
-
+import {
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
+import RegisterComponent from './components/RegisterComponent'
+import LoginComponent from './components/LoginComponent'
 function App() {
 
   return (
     <>
-  <SidenavComponenet/>
+      <Routes>
+        <Route path="/"  element={<SidenavComponenet />}/>
+        <Route path="/register"  element={<RegisterComponent/>}/>
+        <Route path="/login"  element={<LoginComponent/>}/>
+      </Routes>
     </>
   )
 }
